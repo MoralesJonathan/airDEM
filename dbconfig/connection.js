a2592ecab306cc614302b41f00612e7e2139b3d7
Jonathan Morales <jonmorazav@yahoo.com>
2019-02-09 14:41:34 -0500

    fix dotenv per env

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -1,5 +1,5 @@
-const {MongoClient} = require("mongodb"),
-    dotenv = require("dotenv").config(),
-    connection =  new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+const {MongoClient} = require("mongodb");
+process.env.NODE_ENV === "development" ?require("dotenv").config() : null;
+const connection =  new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
     
 module.exports = connection;
