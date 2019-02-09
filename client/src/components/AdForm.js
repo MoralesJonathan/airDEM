3f91722e43ccda3a16690945330d9045e1f3a0c2
Fulton Garcia <fultongarcia@gmail.com>
2019-02-09 14:28:47 -0500

    catching campaign loading error

--- client/src/components/AdForm.js
+++ client/src/components/AdForm.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-import { Container, Form, Row, Col } from "react-bootstrap";
+import { Container, Form, Row, Col, Alert } from "react-bootstrap";
 
 function AdForm() {
-    const [campaigns, setCampaigns] = useState([{name: "Create new Campaign", date: '', markup:''}]);
+    const [campaigns, setCampaigns] = useState([{ name: "Create new Campaign", date: '', markup: '' }]);
+    const [error, setError] = useState(false);
     useEffect(() => {
-        API.getCampaigns().then((res) =>{
+        API.getCampaigns().then((res) => {
             setCampaigns(campaigns.concat(res));
         })
+            .catch((error) => {
+                console.log(error, campaigns);
+                setError(true);
+            })
     }, []);
 
     return (
         <Container>
             <Row>
                 <Col>
+                    {error ? <Alert dismissible variant="danger">There was an error loading your Campaigns</Alert> : null}
                     <Form>
-                    <Form.Group controlId="CampaignForm.CampaignSelect">
+                        <Form.Group controlId="CampaignForm.CampaignSelect">
                             <Form.Label>Select Campaign</Form.Label>
                             <Form.Control as="select">
-                            {campaigns.map((campaign) => (
-                                <option>{campaign.name}</option>             
-                            ))}
+                                {campaigns.map((campaign) => (
+                                    <option>{campaign.name}</option>
+                                ))}
                             </Form.Control>
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlInput1">
