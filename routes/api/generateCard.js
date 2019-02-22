f7600f4d70530b7d1da2f5ecee59455a20d88d90
Jonathan Morales <jonmorazav@yahoo.com>
2019-02-22 00:22:31 -0500

    catch error on image request

--- routes/api/generateCard.js
+++ routes/api/generateCard.js
@@ -3,14 +3,16 @@ const imageGeneration = require("../../services/imageGenerationService");
 const tracking = require("../../controllers/tracking.js");
 
 router.get("/:index/:airline/:lookahead1-:lookahead2/:campaignId", (req, res) => {
-    let {index, airline, campaignId, lookahead1,lookahead2} = req.params;
-    imageGeneration.retrieveImages(index, airline, [lookahead1, lookahead2], campaignId).then(image =>{
+    let { index, airline, campaignId, lookahead1, lookahead2 } = req.params;
+    imageGeneration.retrieveImages(index, airline, [lookahead1, lookahead2], campaignId).then(image => {
         res.writeHead(200, {
             'Content-Type': 'image/jpg',
             'Content-Length': image.length
-          });
-          res.end(image); 
-          tracking.logViewTracking({"campaign":campaignId})
+        });
+        res.end(image);
+        tracking.logViewTracking({ "campaign": campaignId })
+    }).catch(error => {
+        res.send(500);
     })
 })
 
